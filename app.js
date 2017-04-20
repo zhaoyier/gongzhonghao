827b80028880825bca86fa48f1c4badd3eb05b60
zhaojianwei <zhaojianwei@f-road.com.cn>
2017-04-20 20:26:22 +0800

    gongzhonghao

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
-var http = require('http'),
-    wechat = require('node-wechat')("1111");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-http.createServer(function (req, res) {
-    //检验 token
-    wechat.checkSignature(req, res);
-    //预处理
-    wechat.handler(req, res);
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-    //监听文本信息
-    wechat.text(function (data) {
-        //console.log(data.ToUserName);
-        //console.log(data.FromUserName);
-        //console.log(data.CreateTime);
-        //console.log(data.MsgType);
-        //...
-        var msg = {
-            FromUserName : data.ToUserName,
-            ToUserName : data.FromUserName,
-            //MsgType : "text",
-            Content : "这是文本回复",
-            //FuncFlag : 0
-        }
-        //回复信息
-        wechat.send(msg);
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
+}
 
-    //监听图片信息
-    //wechat.image(function (data) { ... });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
-    //监听地址信息
-    //wechat.location(function (data) { ... });
 
-    //监听链接信息
-    //wechat.link(function (data) { ... });
-
-    //监听事件信息
-    //wechat.event(function (data) { ... });
-
-    //监听语音信息
-    //wechat.voice(function (data) { ... });
-
-    //监听视频信息
-    //wechat.video(function (data) { ... });
-
-    //监听所有信息
-    //wechat.all(function (data) { ... });
-}).listen(8080);
+module.exports = app;
